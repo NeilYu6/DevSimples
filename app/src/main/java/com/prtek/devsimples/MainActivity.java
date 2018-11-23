0a4052b3285ebfb7b3f903e3cc99da46e6081749
jarly <yuning@prtek.cn>
2018-11-23 10:30:26 +0800

    setup

---
+++ app/src/main/java/com/prtek/devsimples/MainActivity.java
@@ -0,0 +1,16 @@
+package com.prtek.devsimples;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jarly on 2018/11/23.
+ */
+
+public class MainActivity extends AppCompatActivity {
+  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
